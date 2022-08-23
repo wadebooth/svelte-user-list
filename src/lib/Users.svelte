08c31b6f374a2dfe88c71a40a699a8cc4e91ed56
wadebooth <wademgmt@gmail.com>
2022-08-23 00:05:53 -0400

    updated user list

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,19 +1,32 @@
 <script>
-  let user = {
-    image:'https://cdn-icons-png.flaticon.com/128/3135/3135715.png',
+  import user1 from '../assets/images/user1.png'
+  import user2 from '../assets/images/user2.png'
+  import user3 from '../assets/images/user3.png'
+  let users = [{
+    image: user1,
     name: "John",
-    email: "John@gmail.com"
-  }
+    email: "John@gmail.com",
+  },
+  {
+    image: user3, 
+    name: 'Sara',
+    email: "Sara@yahoo.com",
+  }]
   </script>
   
   
   <div>
     <h1>List of Users</h1>
   <div>
-    <img src={user.image} alt={user.name} />
-    <p>{user.name}</p>
-    <p>{user.email}</p>
+    <img src={users[0].image} alt={users[0].name} />
+    <p>{users[0].name}</p>
+    <p>{users[0].email}</p>
     </div>
+    <div>
+      <img src={users[1].image} alt={users[1].name} />
+      <p>{users[1].name}</p>
+      <p>{users[1].email}</p>
+      </div>
   </div>
   
   
