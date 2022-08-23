30397f8b2621c386eb2d372c6fd546173b4fdef5
wadebooth <wademgmt@gmail.com>
2022-08-23 18:25:38 -0400

    added component props to user list

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -2,40 +2,39 @@
   import user1 from '../assets/images/user1.png'
   import user2 from '../assets/images/user2.png'
   import user3 from '../assets/images/user3.png'
+  import User from './User.svelte'
   let users = [{
     id: 1,
     image: user1,
     name: "John",
     email: "John@gmail.com",
+    active: false
   },
   {
     id: 2,
     image: user2, 
     name: 'Jack',
     email: "Jack@yahoo.com",
+    active: true
   },
 {
   id: 3,
   image: user3,
   name: 'Jim',
-  email: 'jim@grubbrr.com'
+  email: 'jim@grubbrr.com',
+  active: false
 }]
 
   const getUsers = () => {
-    return []
+    return users
   }
   </script>
   
   
   <div>
     <h1 class='text-2xl text-center mt-10'>List of Users</h1>
-    {#each users as { image, name, email, id }, i (id)}
-    <div class='flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center'>
-      <p class='w-10'>{i + 1}</p>
-      <img class='rounded-full w-14 h-14 mx-4' src={image} alt={name} />
-      <p class='mx-4 w-20'>{name}</p>
-      <p class='mx-4 w-20'>{email}</p>
-    </div>
+    {#each getUsers() as user, i (user.id)}
+<User {user} {i}/>
     {:else}
     <p>No user found</p>
   {/each}
