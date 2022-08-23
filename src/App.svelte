11439f8f9d715152db7590c19cd5318d545dac28
wadebooth <wademgmt@gmail.com>
2022-08-22 23:30:09 -0400

    added user object

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
-<div>
-  <h1>List of Users</h1>
-<div>
-  <img src='https://cdn-icons-png.flaticon.com/128/3135/3135715.png' />
-  <p>John</p>
-  <p>John@gmail.com</p>
-  </div>
-</div>
+<script>
+  import Users from './lib/Users.svelte'
+</script>
+
+<Users />
