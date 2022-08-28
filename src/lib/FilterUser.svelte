94da4acc1bb0c40d01e925d9fe147ab031fbe8bb
wadebooth <wademgmt@gmail.com>
2022-08-27 23:44:22 -0400

    updated things

---
+++ src/lib/FilterUser.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    //export let filter
+    const dispatch = createEventDispatcher();
+    
+     const filter = (e) => {
+dispatch('filter', e.target.value)
+     }
+</script>
+
+<div class='ml-4 mt-4'>
+    <label for='user-filter'> Filter User </label>
+    <select 
+    on:change={filter}
+    name='user-filter' 
+    class='border rounded-lg px-4 py-2 ml-4'>
+      <option value='all'>All</option>
+      <option value='active'>Active</option>
+      <option value='inactive'>Inactive</option>
+    </select>
+  </div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -3,6 +3,9 @@
   import user2 from '../assets/images/user2.png'
   import user3 from '../assets/images/user3.png'
   import User from './User.svelte'
+  import FilterUser from './FilterUser.svelte'
+
+
   let users = [{
     id: 1,
     image: user1,
@@ -23,17 +26,30 @@
   name: 'Jim',
   email: 'jim@grubbrr.com',
   active: false
-}]
+}];
 
-  const getUsers = () => {
-    return users
-  }
+let filteredUsers = users;
+
+const filter = ({ detail }) => {
+    if (detail === "null") {
+      filteredUsers = users;
+      return;
+    }
+
+    const active = detail === "true";
+    filteredUsers = users.filter((user) => user.active === active);
+  };
   </script>
   
+  <!-- END OF SCRIPT TAG WITH JAVASCRIPT FUNCTIONS -->
+
   
   <div>
     <h1 class='text-2xl text-center mt-10'>List of Users</h1>
-    {#each getUsers() as user, i (user.id)}
+
+    <FilterUser on:filter{filter} />
+
+    {#each filteredUsers as user, i (user.id)}
 <User {user} {i}/>
     {:else}
     <p>No user found</p>
